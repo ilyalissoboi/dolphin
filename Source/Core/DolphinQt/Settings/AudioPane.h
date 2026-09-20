67c577ca738558d61a8fcad362b848600e968a17
Ilya Lissoboi <ilya.lissoboi@slalom.com>
2026-09-20 23:28:47 +0900

    DolphinQt: define Audio settings in a form

--- Source/Core/DolphinQt/Settings/AudioPane.h
+++ Source/Core/DolphinQt/Settings/AudioPane.h
@@ -3,36 +3,25 @@
 
 #pragma once
 
-#include <array>
+#include <memory>
 
 #include <QWidget>
 
-namespace AudioCommon
+namespace Ui
 {
-enum class DPL2Quality;
+class AudioPane;
 }
-
-class ConfigBool;
-class ConfigChoice;
-class ConfigComplexChoice;
-class ConfigRadioBool;
-class ConfigSlider;
-class ConfigStringChoice;
-class QGroupBox;
-class QHBoxLayout;
-class QLabel;
-class QRadioButton;
-class SettingsWindow;
-class ConfigBool;
 
 class AudioPane final : public QWidget
 {
   Q_OBJECT
 public:
-  explicit AudioPane();
+  explicit AudioPane(QWidget* parent = nullptr);
+  ~AudioPane() override;
 
 private:
-  void CreateWidgets();
+  void BindSettings();
+  void ConfigureLayout();
   void ConnectWidgets();
   void AddDescriptions();
 
@@ -43,40 +32,7 @@ private:
   void UpdateWiimoteRoutingEnabled();
 
   void CheckNeedForLatencyControl();
-  bool m_latency_control_supported;
+  bool m_latency_control_supported = false;
 
-  QHBoxLayout* m_main_layout;
-
-  // DSP Engine
-  ConfigComplexChoice* m_dsp_combo;
-
-  // Volume
-  ConfigSlider* m_volume_slider;
-  QLabel* m_volume_indicator;
-
-  // Backend
-  QLabel* m_backend_label;
-  ConfigStringChoice* m_backend_combo;
-
-  ConfigBool* m_dolby_pro_logic;
-  QLabel* m_dolby_quality_label;
-  ConfigChoice* m_dolby_quality_combo;
-
-  QLabel* m_latency_label;
-  ConfigSlider* m_latency_slider;
-#ifdef _WIN32
-  QLabel* m_wasapi_device_label;
-  ConfigStringChoice* m_wasapi_device_combo;
-#endif
-
-  // Misc Settings
-  ConfigBool* m_audio_fill_gaps;
-  ConfigBool* m_audio_preserve_pitch;
-  ConfigBool* m_speed_up_mute_enable;
-
-  // Wiimote Audio Routing
-  QGroupBox* m_wiimote_routing_box = nullptr;
-  ConfigBool* m_wiimote_routing_enable = nullptr;
-  std::array<ConfigBool*, 4> m_wiimote_output_enable{};
-  std::array<ConfigStringChoice*, 4> m_wiimote_output_device{};
+  std::unique_ptr<Ui::AudioPane> m_ui;
 };
---
+++ Source/QtTests/AudioPaneUiTest.cpp
@@ -0,0 +1,108 @@
+// Copyright 2026 Dolphin Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <QCheckBox>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QSlider>
+#include <QVBoxLayout>
+#include <QWidget>
+
+#include <gtest/gtest.h>
+
+#include "ui_AudioPane.h"
+
+namespace
+{
+QWidget* NextTabFocusWidget(QWidget* widget)
+{
+  QWidget* next = widget;
+  do
+  {
+    next = next->nextInFocusChain();
+  } while (!(next->focusPolicy() & Qt::TabFocus) || widget->isAncestorOf(next));
+  return next;
+}
+}  // namespace
+
+TEST(AudioPaneUiTest, FormOwnsTheAudioSettingsStructure)
+{
+  QWidget pane;
+  Ui::AudioPane ui;
+  ui.setupUi(&pane);
+
+  ASSERT_EQ(ui.rootLayout->count(), 2);
+  EXPECT_EQ(ui.rootLayout->itemAt(0)->layout(), ui.mainColumnLayout);
+  EXPECT_EQ(ui.rootLayout->itemAt(1)->widget(), ui.volumeGroup);
+
+  ASSERT_EQ(ui.mainColumnLayout->count(), 5);
+  EXPECT_EQ(ui.mainColumnLayout->itemAt(0)->widget(), ui.dspGroup);
+  EXPECT_EQ(ui.mainColumnLayout->itemAt(1)->widget(), ui.backendGroup);
+  EXPECT_EQ(ui.mainColumnLayout->itemAt(2)->widget(), ui.playbackGroup);
+  EXPECT_EQ(ui.mainColumnLayout->itemAt(3)->widget(), ui.wiimoteRoutingGroup);
+  EXPECT_NE(ui.mainColumnLayout->itemAt(4)->spacerItem(), nullptr);
+
+  EXPECT_EQ(ui.dspLayout->itemAtPosition(0, 0)->widget(), ui.dspEngineLabel);
+  EXPECT_EQ(ui.dspLayout->itemAtPosition(0, 1)->widget(), ui.dspEngineComboBox);
+  EXPECT_EQ(ui.dspEngineLabel->buddy(), ui.dspEngineComboBox);
+
+  EXPECT_EQ(ui.backendLayout->itemAt(0, QFormLayout::LabelRole)->widget(), ui.backendLabel);
+  EXPECT_EQ(ui.backendLayout->itemAt(0, QFormLayout::FieldRole)->widget(), ui.backendComboBox);
+  EXPECT_EQ(ui.backendLayout->itemAt(1, QFormLayout::LabelRole)->widget(), ui.wasapiDeviceLabel);
+  EXPECT_EQ(ui.backendLayout->itemAt(1, QFormLayout::FieldRole)->widget(), ui.wasapiDeviceComboBox);
+  EXPECT_EQ(ui.backendLayout->itemAt(2, QFormLayout::LabelRole)->widget(), ui.latencyLabel);
+  EXPECT_EQ(ui.backendLayout->itemAt(2, QFormLayout::FieldRole)->widget(), ui.latencySlider);
+  EXPECT_EQ(ui.backendLayout->itemAt(3, QFormLayout::SpanningRole)->widget(),
+            ui.dolbyProLogicCheckBox);
+  EXPECT_EQ(ui.backendLayout->itemAt(4, QFormLayout::LabelRole)->widget(), ui.dolbyQualityLabel);
+  EXPECT_EQ(ui.backendLayout->itemAt(4, QFormLayout::FieldRole)->widget(), ui.dolbyQualityComboBox);
+  EXPECT_EQ(ui.backendLabel->buddy(), ui.backendComboBox);
+  EXPECT_EQ(ui.wasapiDeviceLabel->buddy(), ui.wasapiDeviceComboBox);
+  EXPECT_EQ(ui.latencyLabel->buddy(), ui.latencySlider);
+  EXPECT_EQ(ui.dolbyQualityLabel->buddy(), ui.dolbyQualityComboBox);
+  EXPECT_EQ(ui.dolbyQualityComboBox->count(), 4);
+  EXPECT_EQ(ui.latencySlider->minimum(), 0);
+  EXPECT_EQ(ui.latencySlider->maximum(), 200);
+
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(0, 0)->widget(), ui.audioBufferSizeLabel);
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(0, 1)->widget(), ui.audioBufferSizeSlider);
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(0, 2)->widget(), ui.audioBufferSizeValueLabel);
+  EXPECT_EQ(ui.audioBufferSizeLabel->buddy(), ui.audioBufferSizeSlider);
+  EXPECT_EQ(ui.audioBufferSizeSlider->minimum(), 16);
+  EXPECT_EQ(ui.audioBufferSizeSlider->maximum(), 512);
+  EXPECT_EQ(ui.audioBufferSizeSlider->singleStep(), 8);
+  EXPECT_EQ(ui.audioBufferSizeSlider->pageStep(), 8);
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(1, 0)->widget(), ui.audioFillGapsCheckBox);
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(2, 0)->widget(), ui.audioPreservePitchCheckBox);
+  EXPECT_EQ(ui.playbackLayout->itemAtPosition(3, 0)->widget(), ui.muteOnUnlimitedSpeedCheckBox);
+
+  EXPECT_EQ(ui.wiimoteRoutingLayout->itemAtPosition(0, 0)->widget(), ui.wiimoteRoutingCheckBox);
+  EXPECT_EQ(ui.wiimoteRoutingLayout->itemAtPosition(1, 0)->widget(), ui.wiimote1CheckBox);
+  EXPECT_EQ(ui.wiimoteRoutingLayout->itemAtPosition(1, 1)->widget(), ui.wiimote1DeviceComboBox);
+  EXPECT_EQ(ui.wiimoteRoutingLayout->itemAtPosition(4, 0)->widget(), ui.wiimote4CheckBox);
+  EXPECT_EQ(ui.wiimoteRoutingLayout->itemAtPosition(4, 1)->widget(), ui.wiimote4DeviceComboBox);
+
+  EXPECT_EQ(ui.volumeSlider->orientation(), Qt::Vertical);
+  EXPECT_EQ(ui.volumeSlider->minimum(), 0);
+  EXPECT_EQ(ui.volumeSlider->maximum(), 100);
+  EXPECT_EQ(ui.volumeValueLabel->alignment(), Qt::AlignCenter);
+
+  pane.resize(560, 780);
+  ui.rootLayout->setGeometry(pane.rect());
+  EXPECT_LE(ui.dspGroup->geometry().right(), pane.rect().right());
+  EXPECT_LE(ui.backendGroup->geometry().right(), pane.rect().right());
+  EXPECT_LE(ui.playbackGroup->geometry().right(), pane.rect().right());
+  EXPECT_LE(ui.wiimoteRoutingGroup->geometry().right(), pane.rect().right());
+  EXPECT_LE(ui.volumeGroup->geometry().right(), pane.rect().right());
+
+  EXPECT_EQ(NextTabFocusWidget(ui.dspEngineComboBox), ui.backendComboBox);
+  EXPECT_EQ(NextTabFocusWidget(ui.backendComboBox), ui.wasapiDeviceComboBox);
+  EXPECT_EQ(NextTabFocusWidget(ui.wasapiDeviceComboBox), ui.latencySlider);
+  EXPECT_EQ(NextTabFocusWidget(ui.latencySlider), ui.dolbyProLogicCheckBox);
+  EXPECT_EQ(NextTabFocusWidget(ui.dolbyQualityComboBox), ui.audioBufferSizeSlider);
+  EXPECT_EQ(NextTabFocusWidget(ui.muteOnUnlimitedSpeedCheckBox), ui.wiimoteRoutingCheckBox);
+  EXPECT_EQ(NextTabFocusWidget(ui.wiimote4DeviceComboBox), ui.volumeSlider);
+}
